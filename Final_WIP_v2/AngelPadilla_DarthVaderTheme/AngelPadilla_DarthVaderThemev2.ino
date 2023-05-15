c2359dbb7298da7485156491a099829e2bf305fb
Angel Padilla <angel.padilla@mail.citytech.cuny.edu>
2023-05-14 23:45:37 -0400

    Final WIP 2

---
+++ Final_WIP_v2/AngelPadilla_DarthVaderTheme/AngelPadilla_DarthVaderThemev2.ino
@@ -0,0 +1,102 @@
+//Angel Padilla - Darth Vader Theme 
+
+//So in this sketch I essentially assigned the push button to play the 
+//piezo notes as well as having those notes synced with the LED's. It's a 
+//9 note chiptune melody of the Darth Vader Theme from Star Wars. 
+
+const int buttonPin = 2;
+const int piezo = 8;
+const int LED = 9;
+const int LED2 = 12;
+const int LED3 = 11;
+
+
+int buttonState = 0;
+
+// DEMO FOR SENDING SERIAL DATA FROM ARDUINO TO PROCESSING
+//READS ANALOG SENSOR AND WRITES VALUE TO SERIAL PORT 
+
+
+
+const int SENSOR = A0; //sensor hooked up to analog pin A0
+int val = 0;
+
+
+
+void setup() {
+
+    pinMode(LED, OUTPUT);
+    pinMode(LED2, OUTPUT);
+    pinMode(LED3, OUTPUT);
+    pinMode(buttonPin, INPUT);
+
+    pinMode (SENSOR, INPUT);
+    Serial.begin(9600); // Start serial communication at 9600 baud
+
+}
+
+void loop() {
+
+  val = analogRead(SENSOR); //read sensor and assign to variable called val
+  val = val / 4; //divide val by 4 or remap values to get byte-sized 0-255
+  delay(100); // Wait 100 milliseconds
+
+  /////////Uncomment Serial.print() OR Serial.write() and not both!//////////////
+  
+  //Serial. println(val); //to send human-readable data to Arduino monitor
+  Serial.write(val);  //to send binary data to Processing
+
+  buttonState = digitalRead(buttonPin);
+
+  if (buttonState == HIGH) {
+  digitalWrite(LED, HIGH);
+  tone(piezo, 392.00, 800);
+  delay(700);
+  digitalWrite(LED, LOW);
+  digitalWrite(LED2, HIGH);
+  tone(piezo, 392.00, 800);
+  delay(700);
+  digitalWrite(LED2, LOW);
+  digitalWrite(LED3, HIGH);
+  tone(piezo, 392.00, 800);
+  delay(700);
+  digitalWrite(LED3, LOW);
+  digitalWrite(LED, HIGH);
+  tone(piezo, 311.13, 800);
+  delay(600);
+  digitalWrite(LED, LOW);
+  digitalWrite(LED2, HIGH);
+  tone(piezo, 466.16, 800);
+  delay(600);
+  digitalWrite(LED2, LOW);
+  digitalWrite(LED3, HIGH);
+  tone(piezo, 392.00, 800);
+  delay(700);
+  digitalWrite(LED3, LOW);
+  digitalWrite(LED, HIGH);
+  tone(piezo, 311.13, 800);
+  delay(700);
+  digitalWrite(LED, LOW);
+  digitalWrite(LED2, HIGH);
+  tone(piezo, 466.16, 800);
+  delay(600);
+  digitalWrite(LED2, LOW);
+  digitalWrite(LED3, HIGH);
+  tone(piezo, 392.00, 800);
+  delay(600);
+ 
+
+  digitalWrite(LED, LOW);
+  digitalWrite(LED2, LOW);
+  digitalWrite(LED3, LOW);
+
+
+  
+
+ 
+//I assigned the code so that after the chiptune is played, the LED's all go off
+//at once. This is what I have so far.
+  
+
+  }
+  }
